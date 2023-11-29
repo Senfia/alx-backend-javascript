da07dc7c751c9d4fcc026bc9d015baa04686481e
Senyo Fiawornu <senyo332@gmail.com>
2023-11-29 21:59:50 +0000

    last but one task

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+// a function named guardrail that will accept one argument mathFunction (Function).
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
