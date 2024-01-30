cd094b903adf0e850e762ea218c4cf5da5d80797
root <root@DESKTOP-JKHB9QO>
2024-01-30 21:58:40 +0000

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,22 +1,58 @@
 // File: 7-http_express.js
 
 const express = require('express');
-// const fs = require('fs').promises;
+const fs = require('fs').promises;
 const path = require('path');
-const countStudents = require('./3-read_file_async');
 
 const app = express();
 const PORT = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+// Reimplementing countStudents logic here
+const countStudents = async (dataPath) => {
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8');
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentGroups).includes(field)) {
+        studentGroups[field] = [];
+      }
+      const studentEntries = studentPropNames.map((propName, idx) => [
+        propName, studentPropValues[idx]]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce((pre, cur) => (
+      pre || []).length + cur.length);
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+
+    return true;
+  } catch (error) {
+    console.error('Cannot load the database');
+    throw error;
+  }
+};
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
+app.get('/students', async (_, res) => {
   try {
     const databasePath = path.join(__dirname, 'database.csv');
-    const report = await countStudents(databasePath);
-    const responseText = `This is the list of our students\n${report}`;
+    await countStudents(databasePath);
+    const responseText = 'This is the list of our students';
 
     res.type('text/plain');
     res.send(responseText);
